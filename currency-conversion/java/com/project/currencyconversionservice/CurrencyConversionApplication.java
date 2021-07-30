7a4f360619d43ce5c673930bb23f24605f977571
dbcosta <belindadcosta124@gmail.com>
2021-07-30 19:06:01 +0530

    Initial Commit

---
+++ currency-conversion/java/com/project/currencyconversionservice/CurrencyConversionApplication.java
@@ -0,0 +1,22 @@
+package com.project.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//@EnableFeignClients
+public class CurrencyConversionApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionApplication.class, args);
+	}
+
+}
---
+++ currency-conversion/java/com/project/currencyconversionservice/resource/CurrencyConversion.java
@@ -0,0 +1,18 @@
+package com.project.currencyconversionservice.resource;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class CurrencyConversion {
+
+	private String fromCurrencyCode;
+	private String toCurrencyCode;
+	private BigDecimal exchangeRate;
+	private BigDecimal quantity;	
+	private BigDecimal total;	
+}
